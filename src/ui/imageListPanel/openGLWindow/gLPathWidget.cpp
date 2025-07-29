f5dd886ac5cadd636a802e6e27c270eb0258048f
BlackFrogZQ <1275448553@qq.com>
2025-07-29 15:13:34 +0800

    删除平移、缩放的注释部分

--- src/ui/imageListPanel/openGLWindow/gLPathWidget.cpp
+++ src/ui/imageListPanel/openGLWindow/gLPathWidget.cpp
@@ -55,7 +55,6 @@ namespace TIGER_OpenGL
 		float w = this->width() / m_times / 2;
 		float h = this->height() / m_times / 2;
 		glOrtho(-w - m_viewMove.x(), w - m_viewMove.x(), -h - m_viewMove.y(), h - m_viewMove.y(), -max, max);
-        // glOrtho(-w, w, -h, h, -max, max); // 设置 OpenGL 世界坐标系范围的初始状态
 
         float printRegionSize = 180.0f;
         float half = printRegionSize / 2.0f;
@@ -83,12 +82,6 @@ namespace TIGER_OpenGL
 		m_lastMove = point;
 	}
 
-    // 将 窗口坐标（像素坐标） 转换为 OpenGL世界坐标（绘图坐标）
-    // 这里假设 OpenGL 世界坐标系的原点在窗口中心，x轴向右，y轴向上
-    // m_times 是缩放因子，决定了 OpenGL 世界坐标系的大小
-    // 例如，m_times = 1.0 时，窗口宽度为 960 像素时，OpenGL 世界坐标系的宽度为 960 / 1.0 = 960
-    // 如果 m_times = 2.0，则 OpenGL 世界坐标系的宽度为 960 / 2.0 = 480
-    // 这样可以实现缩放效果
 	QPointF GLPathWidget::getPosition(QPoint p_point)
 	{
 		float x = (p_point.x() - this->width() * 0.5) / m_times;
